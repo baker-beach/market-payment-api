d9f0d6b951e3f6a42bbb7ac2f8b3a9fa9576a7d3
Achim Wolf <achim.wolf@baker-beach.com>
2017-03-13 23:14:47 +0100

    tmp

--- src/main/java/com/bakerbeach/market/payment/api/service/PaymentService.java
+++ src/main/java/com/bakerbeach/market/payment/api/service/PaymentService.java
@@ -3,15 +3,15 @@ package com.bakerbeach.market.payment.api.service;
 import java.math.BigDecimal;
 import java.util.Map;
 
+import com.bakerbeach.market.core.api.model.Cart;
 import com.bakerbeach.market.core.api.model.Customer;
 import com.bakerbeach.market.core.api.model.Order;
 import com.bakerbeach.market.core.api.model.ShopContext;
-import com.bakerbeach.market.core.api.model.XCart;
 import com.bakerbeach.market.payment.api.model.PaymentInfo;
 
 public interface PaymentService {
 	
-	PaymentInfo initPayment(ShopContext shopContext, Customer customer, XCart cart) throws PaymentServiceException;
+	PaymentInfo initPayment(ShopContext shopContext, Customer customer, Cart cart) throws PaymentServiceException;
 	
 	PaymentInfo configPaymentMethod(ShopContext shopContext, Map<String,String> parameters) throws PaymentServiceException;
 	
@@ -19,7 +19,7 @@ public interface PaymentService {
 	
 	PaymentInfo getPaymentInfo(ShopContext shopContext);
 	
-	PaymentInfo doPreOrder(XCart cart, ShopContext shopContext) throws PaymentServiceException;
+	PaymentInfo doPreOrder(Cart cart, ShopContext shopContext) throws PaymentServiceException;
 	
 	PaymentInfo doOrder(Order order) throws PaymentServiceException;
 	
